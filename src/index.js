3e2b42dfb65ff556225728b81dd9b0f199cafae7
Konstantin Berezko <berezkok72@gmail.com>
2022-02-28 19:11:04 +0500

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import './css/style.css';
-import drawLinks from './modules/drawLinks';
-import drawWorks from './modules/drawWorks';
-import arrowTop from './modules/arrowTop';
-
-drawLinks();
-drawWorks();
-arrowTop();
--- src/modules/arrowTop.js
+++
@@ -1,9 +0,0 @@
-const arrowTop = () => {
-    document.getElementById('to-top').addEventListener('click',  () => {
-        document.body.scrollTo({
-            top: 0,
-            behavior: 'smooth',
-        });
-    });
-};
-export default arrowTop;
