172810f4351e50d26257f8206a1b4d419be2915c
Konstantin Berezko <berezkok72@gmail.com>
2022-03-02 12:23:49 +0500

    fix use sass loader

--- src/modules/arrowTop.js
+++ src/modules/arrowTop.js
@@ -1,9 +1,9 @@
 const arrowTop = () => {
-    document.getElementById('to-top').addEventListener('click',  () => {
-        document.body.scrollTo({
-            top: 0,
-            behavior: 'smooth',
-        });
+  document.getElementById('to-top').addEventListener('click', () => {
+    document.body.scrollTo({
+      top: 0,
+      behavior: 'smooth',
     });
+  });
 };
 export default arrowTop;
--- src/modules/drawWorks.js
+++ src/modules/drawWorks.js
@@ -4,41 +4,40 @@ const drawWorks = () => {
   const mainContainer = document.querySelector('.main__container');
 
   mainContainer.innerHTML = works
-    .sort((a, b) => a.id - b.id)
+    // .sort((a, b) => a.id - b.id)
     .reduce((acc, item) => {
       const { id, name, demoUrl, gitUrl, imgSrc, stack, description } = item;
 
       return (
         acc +
         `
-            <!-- ${name} -->
+          <!-- ${name} -->
             <article class="works__box box${id}">
-                <img class="works__box_img" src="${imgSrc}" width="800" height="600" alt="${name}" loading="lazy"/>
-                <h2 class="works__box_title">
-                    <a href="${demoUrl}" aria-label="Перейти на демо-сайт ${name}">
-                        ${name}
-                    </a>
-                </h2>
-                <p class="works__box_description">
-                    ${description}
-                </p>
-                <div class="works__box_stack">
-                    ${stack}
-                </div>
-                <div class="works__box_actions">
-                    <a href="${demoUrl}" class="works__box_btn">
-                        <svg class="demo-${name}">
-                            <use xlink:href="img/icons.svg#view" />
-                        </svg>
-                    </a>
-                    <a href="${gitUrl}" class="works__box_btn">
-                        <svg class="git-${name}">
-                            <use xlink:href="img/icons.svg#github" />
-                        </svg>
-                    </a>
-
-                </div>
-            </article>
+              <img class="works__box_img" src="${imgSrc}" width="800" height="600" alt="${name}" loading="lazy"/>
+              <h2 class="works__box_title">
+              <a href="${demoUrl}" aria-label="Перейти на демо-сайт ${name}">
+                ${name}
+              </a>
+            </h2>
+            <p class="works__box_description">
+              ${description}
+            </p>
+            <div class="works__box_stack">
+              ${stack}
+            </div>
+            <div class="works__box_actions">
+              <a href="${demoUrl}" class="works__box_btn">
+                <svg class="demo-${name}">
+                  <use xlink:href="img/icons.svg#view" />
+                </svg>
+              </a>
+              <a href="${gitUrl}" class="works__box_btn">
+                <svg class="git-${name}">
+                  <use xlink:href="img/icons.svg#github" />
+                </svg>
+              </a>
+            </div>
+          </article>
         `
       );
     }, '');
